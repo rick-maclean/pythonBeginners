1e42822cccb5ec52fd199c2c9e0ed4634e41bf65
Rick MacLean <rick_maclean@sil.org>
2017-05-15 15:04:03 -0500

    more code for this tutorial fileIO

---
+++ fileIO.py
@@ -0,0 +1,16 @@
+import random
+import sys
+import os
+
+test_file = open("test.txt", "wb")
+print(test_file.mode)
+print(test_file.name)
+test_file.write(bytes("Write me to the file oh ya\n", 'UTF-8'))
+test_file.close()
+
+test2_file = open("test.txt", "r+")
+text_in_file = test2_file.read()
+
+print(text_in_file)
+
+#os.remove("test.txt")
--- functions.py
+++ functions.py
@@ -12,24 +12,3 @@ print("what is your name? :")
 #name =sys.stdin.readline()
 #print ('hello ', name)
 
-long_string = "  I'll catch you if you fall - The Floor  "
-print(long_string[0:4])
-
-print(long_string[-5:])
-print(long_string[0:-5])
-print(long_string[:-5])
-print(long_string[0:4] + " be there")
-
-print("%c is my %s letter and my number %d number is %.5f" %
-('J', 'favority', 4, .34))
-
-print(long_string.capitalize())
-print(long_string.find("Floor"))
-print(long_string.isalpha())
-print(long_string.isalnum())
-print(len(long_string))
-print(long_string.replace("Floor", "Ground"))
-print(long_string.strip())
-
-quote_list = long_string.split(' ')
-print(quote_list)
