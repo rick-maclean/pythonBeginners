2afdbf5a1e68da683c5be0714e21867582a5a152
Rick MacLean <maclean@dallas.sil.org>
2017-05-15 12:16:44 -0500

    more code for this tutorial

--- logic.py
+++ logic.py
@@ -4,7 +4,7 @@ import os
 
 # if else elif == != > >= <=
 
-age = 21
+age = 7
 
 if age > 16 :
     print('you are over 16')
@@ -12,3 +12,12 @@ else :
     print('you are 16 or under')
 
 # logical operators    and   or   not   
+
+if ((age >=1) and (age<=18)):
+    print("age >=1 and <=18")
+elif (age ==21) or (age>=65):
+    print ("you blah blah blah")
+elif not(age==30):
+    print("age == 30")
+else:
+    print("this is the else part")
---
+++ looping.py
@@ -0,0 +1,34 @@
+import random
+import sys
+import os
+
+# for x = 0 but not 10,  end="" does not print newLines
+for x in range(0, 10):
+    print(x, ' ', end="")
+
+print('\n') 
+print('Grocery List to be printed next')
+
+grocery_list = ['apples', 'oranges', 'x[rprotein bars', 'cantelope']
+
+for y in grocery_list:
+    print(y)
+
+for x in [2,4,6,8,17]:
+    print(x)
+
+print()
+print("now printing num_list nested loops")
+num_list= [[1,3,5], [10,20,30], [5,10,35]]
+
+for x in range(0, 3):
+    for y in range(0,3):
+        print(num_list[x][y])
+
+
+print()
+print('doing while loop with random numbers')
+random_num = random.randrange(0,100)
+while(random_num !=15):
+    print(random_num)
+    random_num = random.randrange(0,30)
