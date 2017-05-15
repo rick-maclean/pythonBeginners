fe0817bf19a54135f9dbb1f252ebf504bb44b081
Rick MacLean <rick_maclean@sil.org>
2017-05-15 12:32:39 -0500

    more code for this tutorial

---
+++ functions.py
@@ -0,0 +1,9 @@
+import random
+import sys
+import os
+
+def addNumber(fNum, lNum):
+    sunNum = fNum + lNum
+    return sunNum
+
+print(addNumber(1,5))
--- looping.py
+++ looping.py
@@ -32,3 +32,17 @@ random_num = random.randrange(0,100)
 while(random_num !=15):
     print(random_num)
     random_num = random.randrange(0,30)
+
+i=0
+while (i <=20):
+    if(i%2 == 0):
+        print(i)
+    elif(i==9):
+        break
+    else:
+        i += 1
+        continue
+    i = i+1
+
+
+
