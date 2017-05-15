5b7f00b8ff8bd1e28723f0f6fcefd2331bb44c3c
Rick MacLean <rick_maclean@sil.org>
2017-05-15 13:17:38 -0500

    more code for this tutorial strings

--- strings.py
+++ strings.py
@@ -2,16 +2,6 @@ import random
 import sys
 import os
 
-def addNumber(fNum, lNum):
-    sunNum = fNum + lNum
-    return sunNum
-
-print(addNumber(1,5))
-
-print("what is your name? :")
-#name =sys.stdin.readline()
-#print ('hello ', name)
-
 long_string = "  I'll catch you if you fall - The Floor  "
 print(long_string[0:4])
 
